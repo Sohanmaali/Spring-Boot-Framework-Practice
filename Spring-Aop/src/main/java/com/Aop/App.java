3ff4f8aebd7420f42808ce7bc491fa5dff5b5c84
Sohan maali <sohanmaali4@gmail.com>
2024-04-03 18:16:23 +0530

    spring AOP

--- Spring-Aop/src/main/java/com/Aop/App.java
+++ Spring-Aop/src/main/java/com/Aop/App.java
@@ -9,6 +9,8 @@ import com.Aop.services.PaymentServicImp;
  * Hello world!
  *
  */
+
+// MyAspect @Component
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
--- Spring-Aop/src/main/java/com/Aop/aspect/MyAspect.java
+++ Spring-Aop/src/main/java/com/Aop/aspect/MyAspect.java
@@ -4,7 +4,7 @@ import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
-@Component
+//@Component
 public class MyAspect {
 
 //	@Before("execution(* com.Aop.services.payment())")
